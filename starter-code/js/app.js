c712c68b902541ba043681b67bc80042b3e62b4d
eun-n <eunicena.12@gmail.com>
2016-05-25 06:42:13 -0700

    added images

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -10,10 +10,12 @@ redApp.controller('RedCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.title = '';
 	$scope.permalink = '';
 	$scope.author = '';
+	$scope.image = '';
 	$scope.resultsTitles =[];
 	$scope.resultspermas = [];
 	$scope.resultsAuthors = [];
 	$scope.searchHist = [];
+	$scope.resultsImages = [];
 
 
 	//search function
@@ -34,17 +36,23 @@ redApp.controller('RedCtrl', ['$scope', '$http', function($scope, $http) {
 			$scope.resultsTitles =[];
 			$scope.resultspermas = [];
 			$scope.resultsAuthors = [];
+			$scope.resultsImages = [];
 
 			var redData = res.data.data.children;
+			console.log(redData);
 			for(var i=0; i<25; i++) {
 				$scope.title = redData[i].data.title;
 				$scope.author = redData[i].data.author;
 				$scope.permalink = 'http://reddit.com' + redData[i].data.permalink;
+				$scope.image = redData[i].data.url;
 				$scope.resultsTitles.push($scope.title);
 				$scope.resultspermas.push($scope.permalink);
 				$scope.resultsAuthors.push($scope.author);
+				if(redData[i].data.domain == "i.imgur.com") {
+					$scope.resultsImages.push($scope.image);
+				}
 			} 
-		console.log($scope.resultsTitles);
+		// console.log($scope.resultsImages);
 		// console.log($scope.resultspermas);
 		// console.log($scope.resultsAuthors);
 
@@ -64,25 +72,26 @@ redApp.controller('RedCtrl', ['$scope', '$http', function($scope, $http) {
 			$scope.resultsTitles =[];
 			$scope.resultspermas = [];
 			$scope.resultsAuthors = [];
+			$scope.resultsImages = [];
 
 			var redData = res.data.data.children;
 			for(var i=0; i<25; i++) {
 				$scope.title = redData[i].data.title;
 				$scope.author = redData[i].data.author;
 				$scope.permalink = 'http://reddit.com' + redData[i].data.permalink;
+				$scope.image = redData[i].data.thumbnail;
 				$scope.resultsTitles.push($scope.title);
 				$scope.resultspermas.push($scope.permalink);
 				$scope.resultsAuthors.push($scope.author);
+				$scope.resultsImages.push($scope.image);
 			} 
-		// console.log($scope.resultsTitles);
+		console.log($scope.resultsImages);
 		// console.log($scope.resultspermas);
 		// console.log($scope.resultsAuthors);
 
 		}, function error(res) {
 			console.log(res);
 		});
-
-
 	}
 
 	$scope.deletehist = function(bb) {
